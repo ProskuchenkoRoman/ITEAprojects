4f8e33aaf1ba6c336e51691aa75929239508d985
ProskuchenkoRoman <griotman904@gmail.com>
2019-07-28 22:37:30 +0300

    Program which check one bit. Is it setted.

---
+++ Third_task/isbitsetted.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+
+int main() {
+  unsigned long int variable;
+  int shift = 0; // Почему то с char не работает.
+  std::cout << "Please, enter variable and possition of bit which you want know: ";
+  std::cin >> variable;
+  std::cin >> shift;
+  std::cout << "\n";
+  if ((variable >> (shift - 1)) & 1) {
+    std::cout << "Yes";
+  } else {
+    std::cout << "No";
+  }
+}
