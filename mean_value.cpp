e900c43d0e3a38a296519f3f981eb572d5cc13d2
ProskuchenkoRoman <griotman904@gmail.com>
2019-07-28 17:43:22 +0300

    This program computing mean value of input digits.

---
+++ mean_value.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+  unsigned long int variable;
+  std::cout << "Please, enter string of digits for computation of mean value: ";
+  std::cin >> variable;
+  std::cout << "\n";
+  int divider=10;
+  unsigned int result=0;
+  int counter=0;
+  while (true) {
+    if (variable < divider/10) {
+      break;
+    }
+    int tmp = (variable % divider) / (divider / 10);
+    std::cout << tmp << "\n";
+    result = result + tmp;
+    counter++;
+    divider = divider * 10;
+  }
+  std::cout << "Mean value of digits: ";
+  std::cout << ((result + 0.) / counter);
+}
