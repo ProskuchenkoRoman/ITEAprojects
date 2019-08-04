24fa568991e48a0bea0e295d444d26ee34e135bb
ProskuchenkoRoman <griotman904@gmail.com>
2019-08-04 22:43:44 +0300

    Remove code from main + add function.

--- fourth_task/isdigit.cpp
+++ fourth_task/isdigit.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
+void print_all_digits(char* arr);
+
 int main()
 {
   char array[100];
   std::cin >> array;
   int i=0;
   std::cout << "Digits: ";
-    while (array[i] != '\0') {
-      if ((array[i] >= 48) && (array[i] <= 57)) {
-        std::cout << array[i] << " ";
-      }
-      ++i;
+  print_all_digits(array);
+}
+
+void print_all_digits(char* arr) {
+  while (*(arr+i) != '\0') {
+    if ((*(arr+i) >= 48) && (*(arr+i) <= 57)) {
+      std::cout << *(arr+i) << " ";
     }
+    ++i;
+  }
 }
--- fourth_task/revers.cpp
+++ fourth_task/revers.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
+void print_reversed_utterance(char* arr);
+
 int main()
 {
   char arr[100] {};
   int array_len=0;
-  std::cout << "This program reversing input string.\n"
-  std::cout << "Please, enter input string:\n"
+  std::cout << "This program reversing input string.\n";
+  std::cout << "Please, enter input string:\n";
   std::cin >> arr;
+  print_reversed_utterance(arr);
+}
+
+void print_reversed_utterance(char* arr) {
+  int array_len = 0;
   while(arr[array_len] != '\0') {
     ++array_len;
   }
@@ -15,4 +22,3 @@ int main()
     std::cout << arr[array_len--];
   }
 }
-
