3ee8d36569645e999aad1ca31b1b2f617b0e7982
ProskuchenkoRoman <roman.proskuchenko@nikel.loc>
2019-08-07 12:42:18 +0300

    Changes with constants

--- Second_task/Quad_equation.cpp
+++ Second_task/Quad_equation.cpp
@@ -19,15 +19,15 @@ int main() {
   int b = GetCorrectValueFromStream();
   std::cout << "Please, enter 'c' - third coefficient: ";
   int c = GetCorrectValueFromStream();
-  const long long discriminant = pow(b,2) - 4 * a * c;
-  const float divider_of_result = 2.0 * a;
-  if (discriminant > 0) {
-    double result1 = (-b + std::sqrt(discriminant)) / divider_of_result;
-    double result2 = (-b - std::sqrt(discriminant)) / divider_of_result;
+  const long long DISCRIMINANT = pow(b,2) - 4 * a * c;
+  const float DIVIDER_OF_RESULT = 2.0 * a;
+  if (DISCRIMINANT > 0) {
+    double result1 = (-b + std::sqrt(DISCRIMINANT)) / DIVIDER_OF_RESULT;
+    double result2 = (-b - std::sqrt(DISCRIMINANT)) / DIVIDER_OF_RESULT;
     std::cout << "Your result is two variables: x1 = "<< result1 << "\n";
     std::cout << "                              x2 = "<< result2 << "\n";
-  } else if (0 == discriminant) {
-    double result1 = (-b + .0f) / divider_of_result;
+  } else if (0 == DISCRIMINANT) {
+    double result1 = -b / DIVIDER_OF_RESULT;
     std::cout << "Your result is one variables: x = "<< result1 << "\n";
   } else {
     std::cout << "Unfortunately, this expression haven't got solution. Please try again";
