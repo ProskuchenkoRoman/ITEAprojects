6b59d2b23a1645d5e315a4f2d682dec316b8b22a
ProskuchenkoRoman <griotman904@gmail.com>
2019-08-12 09:38:52 +0300

    Program for sum of very big numbers

--- fifth_task/bigsum.cpp
+++ fifth_task/bigsum.cpp
@@ -29,20 +29,20 @@ void revers_array(char* arr) {
   }
 }
 
-void SumOfArray(char* arr1,char* arr2,const unsigned int array_size, unsigned int* LengthOfBiggestArray) {
-  if ((arr1 != nullptr) && (arr2 != nullptr) && (arr1 != arr2)) {
+void SumOfArray(char* arr1,char* arr2,const unsigned int ARRAY_SIZE, unsigned int* LengthOfBiggestArray) {
+  if ((arr1 != nullptr) && (arr2 != nullptr) && (arr1 != arr2) && (LengthOfBiggestArray != nullptr)) {
     revers_array(arr1);
     revers_array(arr2);
     *LengthOfBiggestArray = CmpLengthArray (arr1,arr2);
-    for(uint16_t i = 0; i <= *LengthOfBiggestArray; ++i) {
+    for(unsigned i = 0; i <= *LengthOfBiggestArray; ++i) {
       if (!((arr1[i]>= '0') && (arr1[i] <= '9'))) arr1[i] = '0';
       if (!((arr2[i]>= '0') && (arr2[i] <= '9'))) arr2[i] = '0';
     }
-    for (unsigned int i = 0; i<array_size;++i) {
+    for (unsigned i = 0; i < ARRAY_SIZE;++i) {
       if ((((int)arr1[i]) == 0) && (((int)arr1[i]) == 0)) continue;
       if (arr1[i] != 0) arr1[i] -='0';
       if (arr2[i] != 0) arr2[i] -='0';
-      int tmp = arr1[i]+arr2[i];
+      unsigned tmp = arr1[i]+arr2[i];
       if ((tmp > 9) && (tmp < 20)) {
         tmp -=10;
         arr1[i] = tmp + '0';
@@ -59,15 +59,21 @@ void SumOfArray(char* arr1,char* arr2,const unsigned int array_size, unsigned in
 }
 
 int main() {
-  unsigned int array_size = 100;
+  const unsigned ARRAY_SIZE = 100;
   std::cout << "Please, enter string of digits for sum:\n";
-  char arr1[array_size] {};
-  char arr2[array_size] {};
+  char arr1[ARRAY_SIZE] {};
+  char arr2[ARRAY_SIZE] {};
   std::cin >> arr1;
   std::cout << "+\n";
   std::cin >> arr2;
   std::cout << "=\n";
   unsigned int LengthOfBiggestArray = 0;
-  SumOfArray(arr1, arr2, array_size, &LengthOfBiggestArray);
-  std::cout << arr1;
+  SumOfArray(arr1, arr2, ARRAY_SIZE, &LengthOfBiggestArray);
+  if (!(arr1[0] == '0')) {
+    std::cout << arr1[0];
+  }
+  unsigned i = 1;
+  for (unsigned i = 1; i <= LengthOfBiggestArray; ++i) {
+    std::cout << arr1[i];
+  }
 }
