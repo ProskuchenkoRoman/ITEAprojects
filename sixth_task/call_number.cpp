28b7a097b6d38868040b79d53fd61d3ab9bfd525
ProskuchenkoRoman <griotman904@gmail.com>
2019-08-20 10:18:26 +0300

    Change array to string. Change function to work with string.

--- sixth_task/call_number.cpp
+++ sixth_task/call_number.cpp
@@ -2,49 +2,41 @@
 #include <cstring>
 #include <cmath>
 
-
-void Crypt (char* InputArray, const unsigned NUMBER_OF_PARTS,const unsigned ARRAY_COLUMN, const unsigned ARRAY_LINE) {
-if (InputArray == nullptr) return;
+void Crypt (std::string* InputArrayPtr, const unsigned ARRAY_COLUMN, const unsigned ARRAY_LINE) {
+  if (nullptr == InputArrayPtr) return;
   const unsigned BUF_SIZE = ARRAY_COLUMN * ARRAY_LINE;
-  char buffer[BUF_SIZE] {'0'};
-  for (unsigned offset = 0; offset < NUMBER_OF_PARTS * BUF_SIZE + 1; offset += BUF_SIZE) {
+  const unsigned NUMBER_OF_PARTS = ceil((*InputArrayPtr).length() / (ARRAY_COLUMN * ARRAY_LINE + .0f));
+  (*InputArrayPtr).resize(NUMBER_OF_PARTS * BUF_SIZE);
+  char* buffer = static_cast<char*>(calloc(BUF_SIZE, sizeof(char)));
+  if (nullptr == buffer) {
+    std::cout << "Not enought memory.";
+    return;
+  }
+  for (uint64_t offset = 0; offset < NUMBER_OF_PARTS * BUF_SIZE; offset += BUF_SIZE - 1) {
     for (unsigned i = 0; i < ARRAY_COLUMN; ++i) {
       for (unsigned j = 0; j < ARRAY_LINE; ++j) {
-        buffer[i * ARRAY_COLUMN + j] = InputArray[j * ARRAY_COLUMN + i + offset];
+        buffer[i * ARRAY_COLUMN + j] = (*InputArrayPtr)[j * ARRAY_COLUMN + i + offset];
       }
     }
     for (unsigned i = 0; i < ARRAY_COLUMN; ++i) {
       for (unsigned j = 0; j < ARRAY_LINE; ++j) {
-        InputArray[i * ARRAY_COLUMN + j + offset] = buffer[i * ARRAY_COLUMN + j];
+        (*InputArrayPtr)[i * ARRAY_COLUMN + j + offset] = buffer[i * ARRAY_COLUMN + j];
       }
     }
   }
+  free(buffer);
 }
 
 int main() {
-  const unsigned ARRAY_COLUMN = 4;
-  const unsigned ARRAY_LINE = 4;
+  const unsigned ARRAY_COLUMN = 3;
+  const unsigned ARRAY_LINE = 3;
   std::cout << "Hello! Please enter string which you want to encrypt and decrypt: ";
-  std::string s;
-  std::getline ( std::cin, s );
-  unsigned NumberOfChar = 0;
-  while (s[NumberOfChar] != '\0') {
-    s[NumberOfChar++];
-  }
-  unsigned NumberOfParts = ceil(NumberOfChar / (ARRAY_COLUMN * ARRAY_LINE + .0f));
-  char* ptr = (char *)malloc(NumberOfParts * ARRAY_COLUMN * ARRAY_LINE * sizeof(char));
-  for(uint32_t i = 0; i < NumberOfChar;++i){
-    ptr[i] = s[i];
-  }
-  Crypt (ptr, NumberOfParts, ARRAY_COLUMN, ARRAY_LINE);
-  for (uint32_t i = 0; i < NumberOfParts * ARRAY_COLUMN * ARRAY_LINE; ++i) {
-    std::cout << ptr[i];
-  }
+  std::string str;
+  std::getline ( std::cin, str );
+  Crypt (&str, ARRAY_COLUMN, ARRAY_LINE);
+  std::cout << str;
   std::cout << '\n';
-  Crypt (ptr, NumberOfParts, ARRAY_COLUMN, ARRAY_LINE);
-  for (uint32_t i = 0; i < NumberOfParts * ARRAY_COLUMN * ARRAY_LINE; ++i) {
-    std::cout << ptr[i];
-  }
-  free(ptr);
+  Crypt (&str, ARRAY_COLUMN, ARRAY_LINE);
+  std::cout << str;
 }
 
