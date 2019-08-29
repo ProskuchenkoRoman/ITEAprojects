5050094d9948f6ac0a701c79eeedbe42b03c6c79
ProskuchenkoRoman <griotman904@gmail.com>
2019-08-29 11:51:12 +0300

    Implementation of function bubblesort with lambda-comparator. Changes with fixed types.

---
+++ ninth_task/class_template.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+template<typename Type>
+struct stackcell {
+  Type variable = 0;
+  stackcell* PrevPtr = nullptr; // Вопрос: почему так работает?) stackcell* Оно видит скоуп структуры и работает с названием структуры в скоупе?
+};
+
+template<typename Type>
+class stack {
+  public:
+  Type push(Type InputVar) {
+    stackcell<Type>* NextPtr = (stackcell<Type>*)malloc(sizeof(stackcell<Type>));
+    if (nullptr == NextPtr) {
+      return 0;
+    }
+    (*NextPtr).variable = InputVar;
+    (*NextPtr).PrevPtr = CurrentPtr;
+    CurrentPtr = NextPtr;
+    ++CurrentPosition;
+    return CurrentPosition;
+  }
+  Type pop() {
+    if (nullptr == CurrentPtr) {
+      return 0;
+    }
+    Type Out = (*CurrentPtr).variable;
+    stackcell<Type>* MemoryForFree = CurrentPtr;
+    CurrentPtr = (*CurrentPtr).PrevPtr;
+    free(MemoryForFree);
+    --CurrentPosition;
+    return Out;
+  }
+  Type HowManyElements () const {
+    return CurrentPosition;
+  }
+  private:
+  stackcell<Type>* CurrentPtr = nullptr;
+  unsigned long CurrentPosition = 0;
+};
+
+int main() { 
+  stack<float> stack;
+  std::cout << stack.push(12.1) << '\n';
+  std::cout << stack.push(13.1) << '\n';
+  std::cout << stack.push(14.1) << '\n';
+  std::cout << stack.HowManyElements() << '\n';
+  std::cout << stack.pop() << '\n';
+  std::cout << stack.HowManyElements() << '\n';
+  std::cout << stack.pop() << '\n';
+  std::cout << stack.HowManyElements() << '\n';
+  std::cout << stack.pop() << '\n';
+  std::cout << stack.HowManyElements() << '\n';
+  std::cout << stack.pop() << '\n';
+}
--- ninth_task/func_template.cpp
+++ ninth_task/func_template.cpp
@@ -14,7 +14,7 @@ template <typename Type>
 using lambda = std::function <bool(Type, Type)>;
 
 template <typename Type>
-void bubblesort (Type* array, uint32_t LengthOfArray, lambda<Type> LambdaComparator) {
+void bubblesort (Type* array,const uint32_t LengthOfArray, lambda<Type> LambdaComparator) {
   if ((nullptr == array) || (0 == LengthOfArray)) return;
   bool flag = true;
   while (flag) {
@@ -29,23 +29,23 @@ void bubblesort (Type* array, uint32_t LengthOfArray, lambda<Type> LambdaCompara
 }
 
 int main () {
-  const uint32_t ARRAY_LENGTH = 100;
+  const uint32_t ARRAY_LENGTH = 1000;
   const double PI  =3.141592653589793238463;
-  unsigned array_int[ARRAY_LENGTH] {};
+  uint32_t array_int[ARRAY_LENGTH] {};
   double array_db[ARRAY_LENGTH] {};
   char array_char[ARRAY_LENGTH] {};
-  for (unsigned i = 0; i < ARRAY_LENGTH; ++i) {
+  for (uint32_t i = 0; i < ARRAY_LENGTH; ++i) {
     array_int[i] = rand();
   }
-  for (unsigned i = 0; i < ARRAY_LENGTH; ++i) {
+  for (uint32_t i = 0; i < ARRAY_LENGTH; ++i) {
     array_db[i] = rand()/PI;
   }
-  for (unsigned i = 0; i < ARRAY_LENGTH; ++i) {
+  for (uint32_t i = 0; i < ARRAY_LENGTH; ++i) {
     array_char[i] = static_cast<char>((rand() % 72) + 48);  
   }
   auto LambdaComparator = [](auto a, auto b) {return a > b;};
-  bubblesort<unsigned> (array_int, ARRAY_LENGTH, LambdaComparator);
-  bubblesort<double> (array_db, ARRAY_LENGTH, LambdaComparator);
+  bubblesort<uint32_t> (array_int, ARRAY_LENGTH, LambdaComparator);
+  bubblesort<double> (array_db, ARRAY_LENGTH, LambdaComparator);// фиксированые типы для вещественных чисел есть?
   bubblesort<char> (array_char, ARRAY_LENGTH, [](auto a, auto b) {return ((a < 'A') && (b < 'A') && (a > b)) ||
                                                                          ((a >= 'A') && (b >= 'A') && (a > b)) ||
                                                                          ((a < 'A') && (b >= 'A') && (!(a > b))); });
