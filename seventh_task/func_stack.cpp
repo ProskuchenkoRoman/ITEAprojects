85761d3ac8ed806974c7c699f535177765cdeef8
ProskuchenkoRoman <griotman904@gmail.com>
2019-09-04 22:19:03 +0300

    Work with stack by functions

--- seventh_task/func_stack.cpp
+++ seventh_task/func_stack.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
 
+//g++ -std=c++14 -Wall -Werror -pedantic -O2  func_stack.cpp
+
 struct stackcell {
   long variable = 0;
   stackcell* PrevPtr = nullptr;
 };
 
-void push(long InputVar, stackcell** CurrentPtr) {
-  if (nullptr == CurrentPtr) return;
+void push(const long INPUT_VAR, stackcell*& CurrentPtr) {
+  if (nullptr == CurrentPtr) {
+    CurrentPtr = (stackcell*)malloc(sizeof(stackcell));
+    CurrentPtr->variable = INPUT_VAR;
+    CurrentPtr->PrevPtr = nullptr;
+    return;
+  }
   stackcell* NewCell = (stackcell*)malloc(sizeof(stackcell));
-  NewCell->variable = InputVar;
-  NewCell->PrevPtr = *CurrentPtr;
-  *CurrentPtr = NewCell;
+  NewCell->variable = INPUT_VAR;
+  NewCell->PrevPtr = CurrentPtr;
+  CurrentPtr = NewCell;
 }
 
-long pop(stackcell** CurrentPtr) {
-  long Out = (*CurrentPtr)->variable;
-  *CurrentPtr = (*CurrentPtr)->PrevPtr;
+long pop(stackcell*& CurrentPtr) {
+  if (nullptr == CurrentPtr) {
+    std::cout << "Stack is empty. Try to push something.\n";
+    return false;
+  }
+  long Out = CurrentPtr->variable;
+  stackcell* MemoryForFree = CurrentPtr;
+  CurrentPtr = CurrentPtr->PrevPtr;
+  free(MemoryForFree);
   return Out;
 }
 
-int main() {
-  stackcell* CurrentCell = (stackcell*)calloc(1, sizeof(stackcell));
-  CurrentCell->variable = 0;
-  CurrentCell->PrevPtr = nullptr;
-  
-  push(12, &CurrentCell);
-  std::cout << CurrentCell->variable << std::endl;
-  push(14, &CurrentCell);
-  std::cout << CurrentCell->variable << std::endl;
-  push(45, &CurrentCell);
-  std::cout << CurrentCell->variable << std::endl;
-  push(67, &CurrentCell);
-  std::cout << CurrentCell->variable << std::endl;
+long top(stackcell*& CurrentPtr) {
+  if (nullptr == CurrentPtr) return false;
+  return CurrentPtr->variable;
+}
 
-  std::cout << pop(&CurrentCell) << std::endl;
-  std::cout << pop(&CurrentCell) << std::endl;
-  std::cout << pop(&CurrentCell) << std::endl;
-  std::cout << pop(&CurrentCell) << std::endl;
+int main() {
+  stackcell* CurrentCell = nullptr;
+  pop(CurrentCell);
+  push(12, CurrentCell);
+  std::cout << top(CurrentCell) << std::endl;
+  push(14, CurrentCell);
+  std::cout << top(CurrentCell) << std::endl;
+  push(45, CurrentCell);
+  std::cout << top(CurrentCell) << std::endl;
+  push(67, CurrentCell);
+  std::cout << top(CurrentCell) << std::endl;
+
+  std::cout << pop(CurrentCell) << std::endl;
+  std::cout << pop(CurrentCell) << std::endl;
+  std::cout << pop(CurrentCell) << std::endl;
+  std::cout << pop(CurrentCell) << std::endl;
 }
