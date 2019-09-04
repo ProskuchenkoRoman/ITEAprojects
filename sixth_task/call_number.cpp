6d7c2ffee655f091b65830ea8ac045072d878295
ProskuchenkoRoman <griotman904@gmail.com>
2019-09-04 22:17:10 +0300

    Change logic of work with std::string

--- sixth_task/call_number.cpp
+++ sixth_task/call_number.cpp
@@ -2,41 +2,35 @@
 #include <cstring>
 #include <cmath>
 
-void Crypt (std::string* InputArrayPtr, const unsigned ARRAY_COLUMN, const unsigned ARRAY_LINE) {
-  if (nullptr == InputArrayPtr) return;
-  const unsigned BUF_SIZE = ARRAY_COLUMN * ARRAY_LINE;
-  const unsigned NUMBER_OF_PARTS = ceil((*InputArrayPtr).length() / (ARRAY_COLUMN * ARRAY_LINE + .0f));
-  (*InputArrayPtr).resize(NUMBER_OF_PARTS * BUF_SIZE);
-  char* buffer = static_cast<char*>(calloc(BUF_SIZE, sizeof(char)));
-  if (nullptr == buffer) {
-    std::cout << "Not enought memory.";
-    return;
-  }
-  for (uint64_t offset = 0; offset < NUMBER_OF_PARTS * BUF_SIZE; offset += BUF_SIZE - 1) {
-    for (unsigned i = 0; i < ARRAY_COLUMN; ++i) {
-      for (unsigned j = 0; j < ARRAY_LINE; ++j) {
-        buffer[i * ARRAY_COLUMN + j] = (*InputArrayPtr)[j * ARRAY_COLUMN + i + offset];
-      }
-    }
-    for (unsigned i = 0; i < ARRAY_COLUMN; ++i) {
-      for (unsigned j = 0; j < ARRAY_LINE; ++j) {
-        (*InputArrayPtr)[i * ARRAY_COLUMN + j + offset] = buffer[i * ARRAY_COLUMN + j];
+// g++ -std=c++14 -Wall -Werror -pedantic -O2  call_number.cpp
+
+void Crypt (std::string& InputArray, const uint32_t ARRAY_COLUMN, const uint32_t ARRAY_LINE) {
+  if ((0 == ARRAY_COLUMN) || (0 == ARRAY_LINE) || (0 == InputArray.length())) return;
+  const uint32_t BUF_SIZE = ARRAY_COLUMN * ARRAY_LINE;
+  const uint32_t NUMBER_OF_PARTS = ceil(InputArray.length() / (BUF_SIZE + .0f));
+  InputArray.resize(NUMBER_OF_PARTS * BUF_SIZE);
+  std::string buffer;
+  for (uint64_t offset = 0; offset < NUMBER_OF_PARTS * BUF_SIZE; offset += BUF_SIZE) {
+    buffer.clear();
+    buffer = InputArray.substr(offset,BUF_SIZE);
+    for (uint32_t i = 0; i < ARRAY_COLUMN; ++i) {
+      for (uint32_t j = 0; j < ARRAY_LINE; ++j) {
+        InputArray[i * ARRAY_COLUMN + j + offset] = buffer[j * ARRAY_LINE + i];
       }
     }
   }
-  free(buffer);
 }
 
 int main() {
-  const unsigned ARRAY_COLUMN = 3;
-  const unsigned ARRAY_LINE = 3;
-  std::cout << "Hello! Please enter string which you want to encrypt and decrypt: ";
+  const uint32_t ARRAY_COLUMN = 7;
+  const uint32_t ARRAY_LINE = 7;
+  std::cout << "Hello! Please enter string which you want to encrypt and decrypt:\n";
   std::string str;
   std::getline ( std::cin, str );
-  Crypt (&str, ARRAY_COLUMN, ARRAY_LINE);
+  Crypt (str, ARRAY_COLUMN, ARRAY_LINE);
   std::cout << str;
   std::cout << '\n';
-  Crypt (&str, ARRAY_COLUMN, ARRAY_LINE);
+  Crypt (str, ARRAY_LINE, ARRAY_COLUMN);
   std::cout << str;
 }
 
