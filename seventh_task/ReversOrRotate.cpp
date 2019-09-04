ae48c291c1ae8c00df798bfc9e5ef8cd148bf10e
ProskuchenkoRoman <griotman904@gmail.com>
2019-09-04 23:01:35 +0300

    Optimization of code for ReversOrRotate

--- seventh_task/ReversOrRotate.cpp
+++ seventh_task/ReversOrRotate.cpp
@@ -3,31 +3,40 @@
 #include <string>
 #include <algorithm>
 
+//g++ -std=c++14 -Wall -Werror -pedantic -O2 ReversOrRotate.cpp
+
 using string = std::string;
 
-string revrot(string* str,const int SZ) {
-  if ((SZ <= 0) || (SZ > str->length()) || (0 == str->length()) || (nullptr == str)) return "Fault";
+// что то я не уверен правильно ли я вообще понял задание) отсюда непонятки с кодом)
+
+string revrot(const string& str,const unsigned SZ) {
+  if ((SZ > str.length()) || (0 == str.length())) return "Fault";
+  string rezult;
+  // rezult.resize(str.size());
   uint32_t sum1 = 0;
   uint32_t sum2 = 0;
-  for(size_t i = 0; i < SZ; ++i) {
-    sum1 += pow((int)(str->at(i)-'0'), 2);
+  const uint32_t STR_LEN = str.length();
+  for(size_t i = 0; i < SZ; ++i) { // range-for незнаю как тут использовать, мне ж нужна только часть строки
+    sum1 += pow((int)(str[i] - '0'), 2);
   }
-  for(size_t i = SZ; i < str->length(); ++i) {
-    sum2 += pow((int)(str->at(i)-'0'), 2);
+  for(size_t i = SZ; i < STR_LEN; ++i) {
+    sum2 += pow((int)(str[i] - '0'), 2);
   }
-  if (!(sum1 % 2)) {
-    std::reverse(str->begin(), str->begin() + SZ - 1);
+  if (sum1 % 2) { // я ж правильно понял, условием ротейта и реверса является четность - нечетность?
+    rezult = str.substr(1, SZ - 1);
+    rezult += str[0];
   } else {
-    str->insert(str->cbegin() + SZ, str->at(0));
-    str->erase(0, 1);
+    rezult = str.substr(0 ,SZ);
+    std::reverse(rezult.begin(), rezult.begin() + SZ); //тут я юзаю str->begin() + SZ тк я обрабатую не всю строку а только до SZ
   }
-  if (!(sum2 % 2)) {
-    std::reverse((str->end() - (str->length() - SZ)), str->end());
+  if (sum2 % 2) { // ну мы же вторую половину тоже проверяем на реверс или ротейт
+    rezult += str.substr(SZ + 1, STR_LEN);
+    rezult += str[SZ];
   } else {
-    str->insert(str->end(), str->at(SZ));
-    str->erase(SZ, 1);
+    rezult += str.substr(SZ, STR_LEN);
+    std::reverse(rezult.begin() + SZ, rezult.end());
   }
-  return *str;
+  return rezult;
 }
 
 int main() {
@@ -44,5 +53,5 @@ int main() {
   int sz = 0;
   std::cout << "Please enter length of SZ = ";
   std::cin >> sz;
-  std::cout << "Your result: \n" << revrot(&str, sz);
+  std::cout << "Your result: \n" << revrot(str, sz);
 }
